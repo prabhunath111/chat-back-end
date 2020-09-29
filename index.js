24da3f489f65c72d906d62da15ee6bbb911d83a9
prabhu_nath <prabhu_nath.tiwary@aponiar.com>
2020-09-29 13:12:06 +0530

    pushing code

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
 app.get('/', (req, res) => {
-  // res.sendFile(__dirname + '/index.html');
-  res.send("Hi...");
+  res.sendFile(__dirname + '/index.html');
 });
 io.on('connection', (socket) => {
   socket.on('message', (msg) => {
