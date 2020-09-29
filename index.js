ca92dde169730447479138f8ad2b7e0b030ac731
prabhu_nath <prabhu_nath.tiwary@aponiar.com>
2020-09-29 12:55:24 +0530

    change get method

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+  // res.sendFile(__dirname + '/index.html');
+  res.send("Hi...");
 });
 io.on('connection', (socket) => {
   socket.on('message', (msg) => {
